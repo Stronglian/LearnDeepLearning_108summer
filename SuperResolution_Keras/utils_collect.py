050d92f4a51c7dfe21406c2fb6ed0014a3ff77d8
Stronglian <strong.85.wu@gmail.com>
2019-08-11 22:50:17 +0800

    Create utils_collect.py

---
+++ SuperResolution_Keras/utils_collect.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import os
+#from PIL im
+#%%
+def show_photo(img):
+    plt.figure(figsize=(15, 15))
+    for i in range(1,8):
+        image = img[i,:,:]
+        plt.subplot(8, 8, i)
+        plt.axis('off')
+        plt.imshow(image)
+    plt.show()
+    return
+
+#%%
+    
+def LoadJSON(nameJSON):#, nameDict):
+    #讀取
+    try:
+        with open(nameJSON, 'r') as inputfile:
+            nameDict = json.load(inputfile)
+    except FileNotFoundError:
+        nameDict = dict() #{name:{filename:{"id":,"date":},},}
+    return nameDict
+#%%
+def DumpJSON(nameJSON, nameDict):
+    with open(nameJSON, 'w') as outfile:
+        json.dump(nameDict, outfile)
+    return
+
+def LoadNPY(nameNPY, shape = None):
+    try:
+#        with np.load(nameNPY) as inputfile:
+#            nameArr = inputfile.copy()
+        nameArr = np.load(nameNPY, allow_pickle=True).item()
+    except FileNotFoundError:
+        nameArr = np.zeros(shape)
+    return nameArr
+
+def SaveNPY(nameNPY, nameArr):
+    np.save(nameNPY, nameArr)
+    return
