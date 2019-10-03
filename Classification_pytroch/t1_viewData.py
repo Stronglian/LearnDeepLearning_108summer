c1d62af7ca531297a17c85088005f33d3934224c
Stronglian_PRIA-Z270 <strong.85.wu@gmail.com>
2019-10-03 23:11:57 +0800

    Update t1_viewData.py
    檢視 所有分類

--- Classification_pytroch/t1_viewData.py
+++ Classification_pytroch/t1_viewData.py
@@ -26,5 +26,22 @@ for _n in os.listdir(folderData):
         if d_type == "attribute": # 0.0~1.0
             trainAtt = np.load(folderData+_n) 
     dictData[t_type][d_type] = np.load(folderData+_n)
-            
-show_result_row(trainImg[1150:1155])
+
+#%%
+tmp_i = 10
+show_result_row(trainImg[tmp_i:tmp_i+5])
+
+#%%
+
+tmp_dict_test = dictData["test"]
+
+list_la_pre = []
+class_count = -1
+for _i in range(len(tmp_dict_test["label"])):
+    tmp_la = tmp_dict_test["label"][_i]
+    if tmp_la in list_la_pre:
+        continue
+    list_la_pre.append(tmp_la)
+    class_count += 1
+    print("class:%2d, i:%3d =>"%(class_count, _i), flush=True)
+    show_result_row(tmp_dict_test["image"][_i:_i+2]);
